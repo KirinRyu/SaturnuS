159b380818df033bb1dc346b2256b5d4ca0292e8
Daniel Ryuichi Ferreira Satoy <dan_ryuichi@outlook.com>
2025-04-09 21:41:18 -0300

    ajustes em erros de codigo, cores e estrutura

--- src/App.vue
+++ src/App.vue
@@ -2,13 +2,13 @@
 import { RouterView } from 'vue-router'
 
 /*
-Focar no Operacional, ou seja, criar os produtos e toda a interface de e-commerce.
+  Focar no Operacional, ou seja, criar os produtos e toda a interface de e-commerce.
 
-A partir do momento que a pessoa conectar sua carteira no site, 
-não precisa de mais nada pra comprar tokens.
+  A partir do momento que a pessoa conectar sua carteira no site, 
+  não precisa de mais nada pra comprar tokens.
 
-algebra linear!!!
-gradiente -> vetor tangencial
+  algebra linear!!!
+  gradiente -> vetor tangencial
 */
 </script>
 
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -15,38 +15,40 @@ const closeWalletModal = () => {
 </script>
 
 <template>
-  <div class="items-center !my-8">
-    <img 
-        src="@/assets/img/saturno-logo.png" 
-        alt="Saturno (logo)"
-        class="relative w-lg"
+  <div class=" flex flex-col items-center min-h-screen min-w-screen bg-[#453A62]">
+    <div class="flex items-center !my-8">
+      <img 
+          src="@/assets/img/saturno-logo.png" 
+          alt="Saturno (logo)"
+          class="relative w-lg"
+        >
+    </div>
+  
+    <div class="flex justify-center text-center">
+      <span 
+        @click="openWalletModal" 
+        class="
+          block w-full max-w-50 h-15
+          !font-bold !text-center text-lg p-4 cursor-pointer
+        text-white rounded-xl bg-purple-500/75 ring-2 ring-purple-500/60
+        hover:bg-purple-600/100 transition duration-300 ease-in-out
+        "
+        >
+        Conectar Carteira
+      </span>
+    </div>
+  
+    <Teleport to="#walletSelect">
+      <Transition 
+        enter-active-class="duration-300 ease-out"
+        enter-from-class="opacity-0"
+        enter-to-class="opacity-100"
+        leave-active-class="duration-200 ease-in"
+        leave-from-class="opacity-100"
+        leave-to-class="opacity-0"
       >
+        <WalletSelect :closeWalletModal="closeWalletModal" :wallet-open="walletOpen" />
+      </Transition>
+    </Teleport>
   </div>
-
-  <div class="flex justify-center text-center">
-    <span 
-      @click="openWalletModal" 
-      class="
-        block w-full max-w-50 h-15
-        !font-bold !text-center text-lg p-4 cursor-pointer
-      text-white rounded-xl bg-purple-500/75 ring-2 ring-purple-500/60
-      hover:bg-purple-600/100 transition duration-300 ease-in-out
-      "
-      >
-      Conectar Carteira
-    </span>
-  </div>
-
-  <Teleport to="#walletSelect">
-    <Transition 
-      enter-active-class="duration-300 ease-out"
-      enter-from-class="opacity-0"
-      enter-to-class="opacity-100"
-      leave-active-class="duration-200 ease-in"
-      leave-from-class="opacity-100"
-      leave-to-class="opacity-0"
-    >
-      <WalletSelect :closeWalletModal="closeWalletModal" :wallet-open="walletOpen" />
-    </Transition>
-  </Teleport>
 </template>
