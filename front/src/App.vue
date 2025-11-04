a971daed2d8900626432040122a0c708c4b579e2
KirinRyu <dan_ryuichi@outlook.com>
2025-11-04 13:04:36 -0300

    ajustes de estilização do Transition

--- front/src/App.vue
+++ front/src/App.vue
@@ -5,14 +5,14 @@ import NavBar from './components/NavBar.vue';
 /*
   Focar no Operacional, ou seja, criar os produtos e toda a interface de e-commerce.
 
-  A partir do momento que a pessoa conectar sua carteira no site, 
+  A partir do momento que a pessoa conectar sua carteira no site,
   não precisa de mais nada pra comprar tokens.
-  
+
 */
 </script>
 
 <template>
-  
+
   <NavBar />
 
   <Transition
@@ -22,9 +22,9 @@ import NavBar from './components/NavBar.vue';
   </Transition>
 </template>
 
-<style> 
-.fade-enter-active, .fade-leave-active { 
-  transition: opacity 0.3s ease-in; 
+<style>
+.fade-enter-active, .fade-leave-active {
+  transition: opacity 0.3s ease-in-out;
 }
 
 .fade-enter-from, .fade-leave-to {
